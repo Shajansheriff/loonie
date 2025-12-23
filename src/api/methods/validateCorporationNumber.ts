c2646bc1b386dcff588b32f92b51f0cae24fb68e
Shajahan Akbar Sheriff <Shajansheriff@users.noreply.github.com>
2025-12-22 22:44:16 -0500

    add corporation number validation endpoint

---
+++ src/api/methods/validateCorporationNumber.ts
@@ -0,0 +1,12 @@
+import z from "zod/v4"
+import { api } from "../client"
+
+const validateCorporationNumberResponseSchema = z.object({
+    "corporationNumber": z.string(),
+    "valid": z.boolean(),
+})
+
+
+export const validateCorporationNumber = (corporationNumber: string) => {
+    return api.get(`/corporation-number/${corporationNumber}`, validateCorporationNumberResponseSchema)
+}
